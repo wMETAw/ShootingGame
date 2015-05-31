88fe5cc884ca5359e76ee6365ab4cc4efc3eb0b2
takashima <takashima@cloverlab.jp>
2015-05-31 22:08:45 +0900

    第３回まで

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// speed
+	public float speed = 5;
+	
+	// GameObject
+	public GameObject bullet;
+
+	// start is coroutin
+	IEnumerator Start () { 
+
+		while (true) {
+
+			// bullet is player pos
+			Instantiate (bullet, transform.position, transform.rotation);
+
+			// wait 0.05
+			yield return new WaitForSeconds (0.05f);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		 // right left
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// up down
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// move vector
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		// vector * speed
+		rigidbody2D.velocity = direction * speed;
+	}
+}
